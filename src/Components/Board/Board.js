ea80d87eb27347b83229bcb0d5e4e868641c2c59
suyash9415 <93531625+suyash9415@users.noreply.github.com>
2023-11-03 05:49:40 +0530

    Add files via upload

---
+++ src/Components/Board/Board.js
@@ -0,0 +1,62 @@
+import React, { useState } from "react";
+import { MoreHorizontal, Plus } from "react-feather";
+
+import Card from "../Card/Card";
+import Dropdown from "../Dropdown/Dropdown";
+import Editable from "../Editabled/Editable";
+
+import "./Board.css";
+
+function Board(props) {
+  const [showDropdown, setShowDropdown] = useState(false);
+
+  return (
+    <div className="board">
+      <div className="board_header">
+        <p className="board_header_title">
+          {props.board?.title}
+          <span>{props.board?.cards?.length || 0}</span>
+        </p>
+        <div
+          className="board_header_title_more"
+          onClick={() => setShowDropdown(true)}
+        >
+          
+          <Editable
+          text="+"
+        placeholder="Enter Card Title"
+        displayClass="board_add-card"
+        editClass="board_add-card_edit"
+        onSubmit={(value) => props.addCard(props.board?.id, value)}
+      />
+      <MoreHorizontal />
+          {showDropdown && (
+            <Dropdown
+              class="board_dropdown"
+              onClose={() => setShowDropdown(false)}
+            >
+              <p onClick={() => props.removeBoard()}>Delete Board</p>
+            </Dropdown>
+          )}
+        </div>
+      </div>
+      
+      <div className="board_cards custom-scroll">
+        {props.board?.cards?.map((item) => (
+          <Card
+            key={item.id}
+            card={item}
+            boardId={props.board.id}
+            removeCard={props.removeCard}
+            dragEntered={props.dragEntered}
+            dragEnded={props.dragEnded}
+            updateCard={props.updateCard}
+          />
+        ))}
+       
+      </div>
+    </div>
+  );
+}
+
+export default Board;
